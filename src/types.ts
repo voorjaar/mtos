6f39654c709e56a05425503c72e60c37eeb1573e
voorjaar <felitas@zoho.com>
2022-11-06 15:34:47 +0800

    feat: add types

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface Hooks {
+  getNodeKey?: (node: Node) => any;
+  onBeforeNodeAdded?: (node: Node) => Node;
+  onNodeAdded?: (node: Node) => Node;
+  onBeforeElUpdated?: (fromEl: HTMLElement, toEl: HTMLElement) => boolean;
+  onElUpdated?: (el: HTMLElement) => void;
+  onBeforeNodeDiscarded?: (node: Node) => boolean;
+  onNodeDiscarded?: (node: Node) => void;
+  onBeforeElChildrenUpdated?: (
+    fromEl: HTMLElement,
+    toEl: HTMLElement
+  ) => boolean;
+}
+
+export type Filter = (a: HTMLAnchorElement) => boolean;
